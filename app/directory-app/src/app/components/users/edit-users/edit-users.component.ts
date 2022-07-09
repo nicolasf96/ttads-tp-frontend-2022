4d90f52a3dae136e23bc72de6856c7dc71aa9fc7
Nicolás Fierro <nico.fierro1@gmail.com>
2022-07-09 12:55:50 -0300

    adding photo form in edit-users-component

--- app/directory-app/src/app/components/users/edit-users/edit-users.component.ts
+++ app/directory-app/src/app/components/users/edit-users/edit-users.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ImagesService } from 'src/app/services/images/images.service';
 import { UsersService } from 'src/app/services/users/users.service';
 
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
 @Component({
   selector: 'app-edit-users',
   templateUrl: './edit-users.component.html',
@@ -14,11 +18,15 @@ export class EditUsersComponent implements OnInit {
   identifier = '';
   user: any;
   fileTmp: any;
+  photoSelected: any | ArrayBuffer;
+  file: any | File;
 
   constructor(private userService: UsersService,
     private imageService: ImagesService,
     private route: ActivatedRoute,
     private router: Router) { }
+
+  
 
   ngOnInit(): void {
     this.route.params.subscribe( (params) => this.identifier = params['id'] )
@@ -40,5 +48,17 @@ export class EditUsersComponent implements OnInit {
 
     this.imageService.createImage(body, this.user._id).subscribe(response => console.log(response)); */
   }
+
+  onPhotoSelected($event: any){
+    if ($event.target.files && $event.target.files[0]) {
+      this.file = <File>$event.target.files[0];
+      // image preview
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+
 
 }
